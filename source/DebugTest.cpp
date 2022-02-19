20b132c69c7e989af2602582f4150252b9267125
Sergey Baigudin <baigudin@mail.ru>
2022-02-20 01:04:24 +0300

    Thread detach debug test implemented

--- source/DebugTest.cpp
+++ source/DebugTest.cpp
@@ -7,8 +7,7 @@
  */
 #include "System.hpp"
 #include "lib.Thread.hpp"
-
-#ifdef EOOS_NO_STRICT_MISRA_RULES
+#include <conio.h>               ///< For kbhit() and getch()
 
 namespace eoos
 {
@@ -23,7 +22,9 @@ protected:
         using Parent = ::eoos::Object<>;
       
     public:
-    
+
+        bool_t isDone {false};
+        uint64_t count {0};
         
     private:    
         
@@ -34,6 +35,10 @@ protected:
     
         int32_t start() override
         {
+            while(not isDone)
+            {
+                count++;
+            }
             return 0;
         }
         
@@ -43,23 +48,60 @@ protected:
         }
     
     };
-    
+ 
+    Task task;
+ 
 private:
 
-    System eoos;
+    System eoos_;
 };  
 
-TEST_F(DebugTest, DISABLED_thread)
+TEST_F(DebugTest, DISABLED_threadIsDetached)
 {
-    Task task;
-    lib::Thread<> thread(task);
-    bool volatile isRun {true};
-    while(isRun);
-    thread.execute();
-    isRun = true;
-    while(isRun);
+    uint64_t count[] = {0,0};
+    lib::Thread<>* thread = new lib::Thread<>(task);
+    thread->execute();
+    std::cout << "Thread object is alive..." << std::endl;
+    while(true)
+    {
+        if(::kbhit() != 0)
+        {
+            ::getch();
+            break;
+        }
+        if(task.count > count[0])
+        {
+            std::cout << ".";
+        }
+        count[0] = task.count;
+    }
+    delete thread;
+    std::cout << std::endl << "Thread object is dead on count = " << count[0] << std::endl;
+    count[1] = count[0];
+    while(true)
+    {
+        if(::kbhit() != 0)
+        {
+            ::getch();
+            break;
+        }
+        if(task.count > count[1])
+        {
+            std::cout << ".";
+        }
+        count[1] = task.count;
+    }
+    std::cout << std::endl << "Test is finished on count = " << count[1] << std::endl;
+    if(count[0] == count[1])
+    {
+        std::cout << "Thread was not scheduled after its object had been deleted - it's good case" << std::endl;
+    }
+    else
+    {
+        std::cout << "Thread was detached and continued executing after its object had been deleted - it's bad case" << std::endl;
+    }
+    task.isDone = true;
+    ASSERT_EQ(count[1], count[0]) << "Fatal: Thread stayed in detach mode";
 }
 
 } // namespace eoos
-
-#endif // EOOS_NO_STRICT_MISRA_RULES
